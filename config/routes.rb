45b14805ad7f3d6817c1c466961264294223d94c
Valerii Pohrebniak <km270985pvg@gmail.com>
2025-03-10 11:26:12 +0200

    update routes, rack cors initializer, db schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  devise_for :users, skip: %i[sessions registrations passwords]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_scope :user do
+        post 'sign_up', to: 'registrations#create'
+        post 'sign_in', to: 'sessions#create'
+        delete 'sign_out', to: 'sessions#destroy'
+      end
+      namespace :users do
+        get :show
+      end
+    end
+  end
 end
